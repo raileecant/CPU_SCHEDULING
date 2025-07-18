c4f944716aa978c67cafb9f17abd5e2badc3b92a
raileecant <raileedane.borinaga.23@usjr.edu.ph>
2025-07-18 15:37:24 +0800

    Deleted the old GUI Files because it was not good enough.

--- src/GUI/SchedulerSwing.java
+++
@@ -1,9 +0,0 @@
-package src.GUI;
-
-import javax.swing.*;
-
-public class SchedulerSwing {
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new SchedulerFrame().setVisible(true));
-    }
-}
